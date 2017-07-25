d9ffa2836591cd34e77877b8f5d3262a1e979bc4
Hack Reactor Student <student@hackreactor.com>
2017-07-24 20:39:04 -0700

    linked to server and mostly though psot request

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,3 +1,5 @@
+const database = require('./database/data.js');
+const fs = require('fs');
 /*************************************************************
 
 You should implement your request handler function in this file.
@@ -11,6 +13,84 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+
+var defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept, X-Parse-Application-Id, X-Parse-REST-API-Key',
+  'access-control-max-age': 10, // Seconds.
+};
+
+
+
+var MessageCreater = (body) => {
+  var record = {
+    username: body.username,
+    roomname: body.roomname,
+    text: body.text,
+    createdAt: new Date().toISOString(),
+    modifiedAt: new Date().toISOString()
+  };
+  
+  return record;
+};
+
+
+var getRequest = (request, response) => {
+
+  //404 for non-existent endpoint
+  request.headers;
+  //Split into useable format;
+  //http://127.0.0.1:8080/?username=Batman
+
+  response.end(JSON.stringify(testbody));
+};
+
+var postRequest = (request, response) => {
+  console.log("post requewust recievedd3edd 34");
+  // request.headers;
+  //404 for non-existent
+  var uri = request.url.split('/');
+  var size = uri.length;
+  var statusCode;
+  if (database[uri[size - 2 ]][uri[size - 1]] === undefined) {
+    statusCode = 404;
+    response.end();
+  } 
+  
+  console.log('body is', request);
+  database[uri[size - 2]][uri[size - 1]].push(MessageCreater(request.body));
+    
+  response.end();
+};
+var putRequest = (request, response) => {
+  request.headers;
+  //404 for non-existent
+
+
+
+  response.end();
+};
+var optionRequest = (request, response) => {
+  request.headers;
+  //404 for non-existent
+  response.end();
+};
+var deleteRequest = (request, response) => {
+  request.headers;
+  //404 for non-existent
+
+  response.end();
+};
+
+
+//Post Function
+//Get Function
+//Option Function
+//Put Function
+//Get Function
+//Date Function
+
 
 var requestHandler = function(request, response) {
   // Request and Response come from node's http module.
@@ -28,10 +108,24 @@ var requestHandler = function(request, response) {
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  console.log('Request Headers' + JSON.stringify(request.headers));
+  console.log('Response Items' + response);
 
   // The outgoing status.
   var statusCode = 200;
 
+  var routing = {
+    POST: postRequest,
+    GET: getRequest,
+    PUT: putRequest,
+    OPTION: optionRequest,
+    DELETE: deleteRequest
+  };
+
+  if (request.method === 'POST') {
+    
+    routing[request.method](request, response);
+  }
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
 
@@ -39,7 +133,7 @@ var requestHandler = function(request, response) {
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
+  headers['Content-Type'] = 'application/json';
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
@@ -49,11 +143,17 @@ var requestHandler = function(request, response) {
   // anything back to the client until you do. The string you pass to
   // response.end() will be the body of the response - i.e. what shows
   // up in the browser.
-  //
+  var body = ('StatusCode' + statusCode + ', Headers:' + JSON.stringify(headers));
+
+  
+
+
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  response.end(JSON.stringify(database['classes']['messages']));
 };
+
+
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 // This code allows this server to talk to websites that
@@ -64,10 +164,6 @@ var requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
+
+module.exports.requestHandler = requestHandler;
 
